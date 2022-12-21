dac91b46f559a042da3990412e9a35a172fb46ae
Pawan <Pawanrewatkar28@gmail.com>
2022-12-21 11:48:19 +0530

    First commit...

---
+++ src/com/testClass/Test1.java
@@ -0,0 +1,15 @@
+package com.testClass;
+
+public class Test1 {
+public static void main(String[] args) {
+	
+	String hungry="Yes";
+	
+	if(hungry=="Yes"){
+		System.out.println("eat something...");
+	}else{
+		System.out.println("Do work..");
+	}
+}
+	
+}
